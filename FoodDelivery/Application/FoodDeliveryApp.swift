b8c50ad52f4c84061b66e243b389bd5c0831a1a5
vivek8421 <vivek.lokhande@neosoftmail.com>
2024-10-29 14:14:57 +0530

    set up

---
+++ FoodDelivery/Application/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Neosoft on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
